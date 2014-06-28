b61524a544d449d32919829a1b8d17e5cfe5f4c5
Phil Hack <phil@philhack.com>
2014-06-28 12:36:18 +0100

    corrected issue with using floats (based on errata)

--- ProductSearch.Core/ProductRepository.cs
+++ ProductSearch.Core/ProductRepository.cs
@@ -7,9 +7,9 @@ namespace ProductSearch.Core
 	public class ProductRepository
 	{
 		private Product[] products = new [] {
-			new Product { Id = 1, Name = "Shoes", Price = 19.99m },
-			new Product { Id = 2, Name = "Shirt", Price = 15.99m },
-			new Product { Id = 3, Name = "Hat", Price = 9.99m }
+			new Product { Id = 1, Name = "Shoes", Price = 19.99f },
+			new Product { Id = 2, Name = "Shirt", Price = 15.99f },
+			new Product { Id = 3, Name = "Hat", Price = 9.99f }
 		};
 
 		public async Task<Product[]> SearchProducts(string searchTerm){
